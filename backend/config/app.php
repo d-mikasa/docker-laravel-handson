abb8f5e7af57df97cdfe3ac73d0aab37441c85e8
mika <d.mikasa@ebacorp.jp>
2020-12-04 14:10:40 +0900

    form-request

--- backend/config/app.php
+++ backend/config/app.php
@@ -175,6 +175,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        //教本で入れろと言われたサービスプロバイダ
+        App\Providers\HelloServiceProvider::class
     ],
 
     /*
