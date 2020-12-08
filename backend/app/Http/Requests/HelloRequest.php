9715b18facbe98fef619895b859290177e601666
mika <d.mikasa@ebacorp.jp>
2020-12-08 12:01:54 +0900

    db

--- backend/app/Http/Requests/HelloRequest.php
+++ backend/app/Http/Requests/HelloRequest.php
@@ -6,25 +6,32 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class HelloRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
     public function authorize()
     {
-        return false;
+        if($this->path() == 'hello'){
+            return true;
+        } else {
+            return false;
+        }
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array
-     */
     public function rules()
     {
         return [
-            //
+            'name' => 'required',
+            'mail' => 'email',
+            'age' => ['numeric', new Myrule(5)],
         ];
     }
+
+    public function messages()
+    {
+        return [
+            'name.required' => '名前は必ず入力してください。',
+            'mail.email' => 'メールアドレスが必要です。',
+            'age.numeric' => '年齢を整数で記入してください。',
+            'age.hello' => 'HELLO! 入力は偶数のみ受け付けます。',
+        ];
+    }
+
 }
