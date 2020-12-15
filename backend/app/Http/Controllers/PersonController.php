bc77c4ae4ab7fd1638a9739cf6be5f9d6efc56ff
mika <d.mikasa@ebacorp.jp>
2020-12-15 17:29:34 +0900

    orm

--- backend/app/Http/Controllers/PersonController.php
+++ backend/app/Http/Controllers/PersonController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Person;
 use Illuminate\Http\Request;
 
-
 class PersonController extends Controller
 {
     public function index(Request $request)
--- backend/app/Person.php
+++ backend/app/Person.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Person extends Model
 {
-    //
+    // protected $table = 'hoge';
+    public function getData()
+{
+
+    return $this -> id . ':' . $this->name . '(' . $this->age . ')';
+}
 }
